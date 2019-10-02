50f5633cb751739ae945b2a4c2d599b74acf8a49
domingoarb <r.domingoalberto@gmail.com>
2019-10-01 22:03:09 -0400

    Updates

---
+++ precache-manifest.1ebffa02413545f95f201b804778ff7a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3458bf9475d8b71b619a8877d4bd5eb4",
+    "url": "/domingoarb/react_test_robot/index.html"
+  },
+  {
+    "revision": "9fe13d1ccdb62f2d8c71",
+    "url": "/domingoarb/react_test_robot/static/css/2.d284fd02.chunk.css"
+  },
+  {
+    "revision": "f7cfd2a74c75a07fb036",
+    "url": "/domingoarb/react_test_robot/static/css/main.ab728e17.chunk.css"
+  },
+  {
+    "revision": "9fe13d1ccdb62f2d8c71",
+    "url": "/domingoarb/react_test_robot/static/js/2.791d43d6.chunk.js"
+  },
+  {
+    "revision": "f7cfd2a74c75a07fb036",
+    "url": "/domingoarb/react_test_robot/static/js/main.2ae70256.chunk.js"
+  },
+  {
+    "revision": "5094edbbadfcf33e6eef",
+    "url": "/domingoarb/react_test_robot/static/js/runtime-main.c14dab77.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/domingoarb/react_test_robot/static/media/SEGA.12a3db8b.woff"
+  }
+]);
