9b496dccddb932c89e0085c33766137f739761b2
domingoarb <r.domingoalberto@gmail.com>
2019-10-01 22:07:42 -0400

    Updates

---
+++ precache-manifest.8c04774aa67b1417fb43df880caeb772.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65b4732cdfa45b9adf807589712f9898",
+    "url": "/react_test_robot/index.html"
+  },
+  {
+    "revision": "9fe13d1ccdb62f2d8c71",
+    "url": "/react_test_robot/static/css/2.d284fd02.chunk.css"
+  },
+  {
+    "revision": "7b7068311de5232bb59d",
+    "url": "/react_test_robot/static/css/main.01232d45.chunk.css"
+  },
+  {
+    "revision": "9fe13d1ccdb62f2d8c71",
+    "url": "/react_test_robot/static/js/2.791d43d6.chunk.js"
+  },
+  {
+    "revision": "7b7068311de5232bb59d",
+    "url": "/react_test_robot/static/js/main.9648e990.chunk.js"
+  },
+  {
+    "revision": "a8bdb7f834218123c060",
+    "url": "/react_test_robot/static/js/runtime-main.b0de8c31.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/react_test_robot/static/media/SEGA.12a3db8b.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/domingoarb/react_test_robot/precache-manifest.1ebffa02413545f95f201b804778ff7a.js"
+  "/react_test_robot/precache-manifest.8c04774aa67b1417fb43df880caeb772.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/domingoarb/react_test_robot/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react_test_robot/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
